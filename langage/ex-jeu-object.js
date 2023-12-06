bee86ea43fda61a5f66616614cc70c048d7555f3
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-12-06 11:12:28 +0100

    Correction objet

--- langage/ex-jeu-object.js
+++ langage/ex-jeu-object.js
@@ -1,13 +1,35 @@
 // Reprendre le code de ex-jeu.js
 
 // Exercice 1
-// En vous inspirant de MyMath de 08-object.js
+// En vous inspirant de MyMath de 07-object.js
 // Créer un namespace object Random qui regroupe
 // les 4 fonctions (de la correction)
 // On écrira Random.getRandomInt(0, 100) pour l'appel
+// Pour rappel aujourd'hui c'est une mauvaise pratique
+// il vaut les 4 fonctions
+const Random = {
+  getRandom() {
+    return Math.random();
+  },
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  },
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+};
+
+const readline = require('readline');
 
 // Exercice 2
-// Créer un classe Jeu dont le constructeur
+// Créer une classe Jeu dont le constructeur
 // va créer les 3 propriétés suivantes :
 // - rl (reprendre le code de ex-jeu)
 // - entierAlea (créé à partir de Random)
@@ -18,6 +40,48 @@
 // const game = new Jeu();
 // game.jouer()
 
+class Jeu {
+  constructor(options = {}) {
+    // const min = (options.min !== undefined) ? options.min : 0;
+    const min = options.min ?? 0;
+    const max = options.max ?? 100;
+    this.rl = readline.createInterface({
+      input: process.stdin, // le clavier
+      output: process.stdout, // le terminal
+    });
+    this.entierAlea = Random.getRandomInt(min, max);
+    this.essais = [];
+  }
+  jouer() {
+    if (this.essais.length) {
+      console.log('Vous avez déjà saisi ' + this.essais.join(' - '));
+    }
+
+    this.rl.question('Quel est le nombre ? ', (answer) => {
+
+      const entierSaisi = Number.parseInt(answer, 10);
+
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Erreur : il faut saisir un nombre');
+        return this.jouer();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit');
+        this.jouer();
+      } else if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand');
+        this.jouer();
+      } else {
+        console.log('Gagné');
+        this.rl.close();
+      }
+    });
+  }
+}
+
 // Exercice 3
 // Modifier le constructeur de Jeu
 // de sorte à ce qu'on puisse lui passer un objet avec des paramètres
@@ -27,3 +91,7 @@
 
 // Prévoir des valeurs par défaut pour min et max
 // Prévoir qu'on puisse appeler Jeu sans paramètre comme dans l'ex 2
+
+const game = new Jeu();
+game.jouer();
+
